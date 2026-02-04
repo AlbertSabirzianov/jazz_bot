dd4e079d38e174b84038b50c1fc3d01b385863d2
AlbertSabirzianov <albertuno@mail.ru>
2026-02-04 12:32:14 +0300

    добавил скрытие браузера при парсинге

--- src/app/interfaces.py
+++ src/app/interfaces.py
@@ -35,7 +35,7 @@ class SDriver:
         options = Options()
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
 
         self.driver = webdriver.Chrome(
             service=ChromeService(ChromeDriverManager().install()),
